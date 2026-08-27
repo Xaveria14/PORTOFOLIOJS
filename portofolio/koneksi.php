27f2c4b3bf63b52dbd240dfa4d7bdb23527a2207
Xaveria14 <fputrinata@gmail.com>
2026-08-27 09:59:29 +0700

    porto

---
+++ portofolio/koneksi.php
@@ -0,0 +1,19 @@
+<?php
+
+$host = "localhost";
+$username = "root";
+$password = "";
+$database = "id";
+
+$conn = mysqli_connect(
+    $host,
+    $username,
+    $password,
+    $database
+);
+
+if (!$conn) {
+    die("Koneksi gagal: " . mysqli_connect_error());
+}
+
+?>
